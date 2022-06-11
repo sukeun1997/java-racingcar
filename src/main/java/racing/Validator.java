bbe64f9962e3463e104ee3e9b2f69507df378a10
sukeun1997 <busan-sy@naver.com>
2022-06-11 16:01:06 +0900

    Refactor : Validator 리팩토링 및 테스트 수정

--- src/main/java/racing/Validator.java
+++ src/main/java/racing/Validator.java
@@ -6,12 +6,16 @@ public class Validator {
 
     private static final Pattern INPUT_PATTERN = Pattern.compile("[[가-힣a-zA-Z0-9]+,]*[가-힣a-zA-Z0-9]+");
 
-    public static boolean validateInput(String inputs) {
-        return INPUT_PATTERN.matcher(inputs).matches();
+    public static void validateInput(String inputs) {
+        if (!INPUT_PATTERN.matcher(inputs).matches()) {
+            throw new IllegalArgumentException("옳바른 자동차 이름을 입력해주세요");
+        }
     }
 
-    public static boolean validateName(String carName) {
-        return carName.length() <= 5;
+    public static void validateName(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5글자를 초과할 수 없습니다.");
+        }
     }
 
 
--- src/test/java/racing/ValidatorTest.java
+++ src/test/java/racing/ValidatorTest.java
@@ -1,10 +1,10 @@
 package racing;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ValidatorTest {
 
@@ -14,43 +14,42 @@ class ValidatorTest {
     @Test
     @DisplayName("자동차 이름에 잘 못된 구분자 입력")
     void test_validateInput_01() {
-        boolean result = Validator.validateInput("a,b.c");
-        assertThat(result).isFalse();
+        assertWrongCarNames("a,b.c");
     }
 
     @Test
     @DisplayName("자동차 이름 알맞은 구분자")
     void test_validateInput_02() {
-        boolean result = Validator.validateInput("a,b,c");
-        assertThat(result).isTrue();
+     Validator.validateInput("a,b,c");
     }
 
     @Test
     @DisplayName("자동차 이름 하나만 입력")
     void test_validateInput_03() {
-        boolean result = Validator.validateInput("car");
-        assertThat(result).isTrue();
+        Validator.validateInput("car");
     }
 
     @Test
     @DisplayName("자동차 이름 끝에 구분자 입력")
     void test_validateInput_04() {
-        boolean result = Validator.validateInput("car,");
-        assertThat(result).isFalse();
+        assertWrongCarNames("car,");
     }
 
     @Test
     @DisplayName("자동차 이름 공백")
     void test_validateInput_05() {
-        boolean result = Validator.validateInput("");
-        assertThat(result).isFalse();
+        assertWrongCarNames("");
     }
 
     @Test
     @DisplayName("알파벳이 아닌 숫자 및 한글로 자동차 이름 입력")
     void test_validateInput_06() {
-        boolean result = Validator.validateInput("자동차01");
-        assertThat(result).isTrue();
+        Validator.validateInput("자동차01");
+    }
+
+    private void assertWrongCarNames(String inputs) {
+        assertThatThrownBy(() -> Validator.validateInput(inputs))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     /**
@@ -60,13 +59,14 @@ class ValidatorTest {
     @DisplayName("5글자 이하 입력")
     void test_validateName_01() {
         String carName = "car1";
-        assertThat(Validator.validateName(carName)).isTrue();
+        Validator.validateName(carName);
     }
 
     @Test
     @DisplayName("5글자 초과 입력")
     void test_validateName_02() {
         String carName = "car1234";
-        assertThat(Validator.validateName(carName)).isFalse();
+        assertThatThrownBy(() -> Validator.validateName(carName))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
